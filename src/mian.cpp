0efd21bef0c1cc7857fa85fe8f1d0f72d00a4d7e
Fanxy2 <65218538+Fanxy2@users.noreply.github.com>
2020-06-08 02:15:27 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -4,14 +4,15 @@
 #pragma warning(disable : 4996)
 #ifdef this_is_a_great_codo
 #endif
-int main() {
+int main() 
+{
   char mojiretsu[1000];
-  for（int mojisuu = 1;mojisuu <=1000;mojisuu++）
+  for（int mojisuu = 1;mojisuu <= 1000;mojisuu++）
   {
     if (mojisuu % 15 == 0) printf("FizzBuzz, ");
   else if (mojisuu % 3 == 0) printf("Fizz, ");
   else if (mojisuu % 5 == 0) printf("Buzz, ");
   else if printf("mojisuu, ");
- }
+  }
   return 0;
 }
