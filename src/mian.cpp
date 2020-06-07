2823136bfd7a9a4d1cbd842ea12678b9077823c2
Fanxy2 <65218538+Fanxy2@users.noreply.github.com>
2020-06-08 02:40:12 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -5,8 +5,10 @@
 #pragma warning(disable : 4996)
 #ifdef this_is_a_great_codo
 #endif
-int main() {
-  for（int n = 1;n <= 1000;n++）{
+int main() 
+{
+  for(int n = 1;n <= 1000;n++)
+  {
     if (n % 15 == 0) std::cout<<"FizzBuzz,";
   else if (n % 3 == 0) std::cout<<"Fizz,";
   else if (n % 5 == 0) std::cout<<"Buzz,";
