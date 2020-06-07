bcc25026a8168d8f82fb3ebcded2c63112e3e5f8
Fanxy2 <65218538+Fanxy2@users.noreply.github.com>
2020-06-07 18:18:24 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -5,13 +5,8 @@
 #ifdef this_is_a_great_codo
 #endif
 int main() {
-char mojiretsu[1000];
-for（int n = 1；n <=1000;n++）
-if swtich(n){
-case(n%15=0):cout<<“FizzBuzz”<<“,”;break;
-case(n%3=0):cout<<“Fizz”<<“,”;break;
-case(n%5=0):cout<<“Buzz”<<“,”;break;
-  default:cout<<n<<“,”;break;
- }
-  return 0;
-}
+  char mojiretsu[1000];
+int mojisuu = 0;
+  while (mojisuu++ < 1000)
+  sprintf(mojiretsu, "%d", mojisuu), 
+  printf("%s, ", mojisuu % 15 ? mojisuu % 3 ? mojisuu % 5 ? mojiretsu : "Buzz" : "Fizz" : "FizzBuzz"); system("PAUSE"); }
