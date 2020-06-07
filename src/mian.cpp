37cbe009e2a78e86324438b3282528689a5efe65
Fanxy2 <65218538+Fanxy2@users.noreply.github.com>
2020-06-08 02:09:33 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -8,10 +8,10 @@ int main() {
   char mojiretsu[1000];
   for（int mojisuu = 1；mojisuu <=1000;mojisuu++）
   {
-    if (mojisuu%15 == 0) cout<<"FizzBuzz,";
-  else if (mojisuu%3 == 0) cout<<"Fizz,";
-  else if (mojisuu%5 == 0) cout<<"Buzz,";
-  else if cout<<"miojisuu,";
+    if (mojisuu%15 == 0) printf("FizzBuzz, ");
+  else if (mojisuu%3 == 0) printf("Fizz, ");
+  else if (mojisuu%5 == 0) printf("Buzz, ");
+  else if printf("mojisuu, ");
  }
   return 0;
 }
